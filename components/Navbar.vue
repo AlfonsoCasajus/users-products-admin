9953a65ae5efe5f588b92895260cde5ccdd9b7c8
Alfonso Casajus <60479980+AlfonsoCasajus@users.noreply.github.com>
2025-01-31 21:27:36 -0300

    icons and images

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,8 @@
 <template>
-    <nav class="bg-[#3BCDBD] p-4">
+    <nav class="bg-[#212529] p-2">
         <div class="flex items-center justify-between">
             <div class="flex items-center">
-                <a class="text-white font-bold text-xl" href="#">
-                    Logo
-                </a>
+                <NuxtImg src="dapteeHead.png" alt="Logo" width="50" height="50" format="webp" quality="80" />
             </div>
             <div class="flex items-center space-x-4">
                 <nuxt-link to="/login" class="text-gray-800 hover:text-blue-500">Salir</nuxt-link>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,12 +1,16 @@
 <template>
-    <div class="w-32 h-full bg-[#3BCDBD] p-4">
+    <div class="h-full bg-[#3BCDBD] p-4">
         <ul>
             <li class="mb-4">
-                <nuxt-link to="/users" class="text-gray-800 hover:text-blue-500">Usuarios</nuxt-link>
+                <nuxt-link to="/users" class="text-gray-800 hover:text-blue-500"><IconUsers size="32"/></nuxt-link>
             </li>
             <li class="mb-4">
-                <nuxt-link to="/products" class="text-gray-800 hover:text-blue-500">Productos</nuxt-link>
+                <nuxt-link to="/products" class="text-gray-800 hover:text-blue-500"><IconBox size="32"/></nuxt-link>
             </li>
         </ul>
     </div>
 </template>
+
+<script setup lang="ts">
+import { IconBox, IconUsers } from '@tabler/icons-vue';
+</script>
