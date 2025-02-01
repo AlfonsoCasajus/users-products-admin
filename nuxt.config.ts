948133c479e06b11927bd09377477b2be459c28a
Alfonso Casajus <60479980+AlfonsoCasajus@users.noreply.github.com>
2025-02-01 11:21:55 -0300

    add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,31 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-
+  build: {
+    transpile: ['vuetify'],
+  },
   vite: {
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    },
     plugins: [
       tailwindcss(),
     ],
   },
 
-  modules: ['@nuxt/image']
+  modules: [
+    '@nuxt/image',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    }
+  ]
 })
