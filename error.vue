5c350affd054a734d4ac51f4e06dbc144d32c092
Alfonso Casajus <60479980+AlfonsoCasajus@users.noreply.github.com>
2025-02-03 09:25:37 -0300

    404 page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-column justify-center align-center">
+        <v-empty-state
+            headline="Whoops, 404"
+            title="Pagina no encontrada"
+            text="La pagina que estabas buscando no existe"
+            image="logoDaptee.svg"
+        />
+
+        <v-btn
+            to="/users"
+            variant="tonal">
+            Volver a Usuarios
+        </v-btn>
+    </div>
+</template>
