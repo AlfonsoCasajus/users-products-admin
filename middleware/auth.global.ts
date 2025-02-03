056fb16fca12831604499b118f2e0b29cb1a4150
Alfonso Casajus <60479980+AlfonsoCasajus@users.noreply.github.com>
2025-02-03 16:36:17 -0300

    fix home route middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,9 +3,10 @@ export default defineNuxtRouteMiddleware((to, from) => {
  const authToken = localStorage.getItem('auth-token');
  const isAuthenticated = Boolean(authToken);
 
+ // Si no esta autenticado se lo envia al login
   if (!isAuthenticated && to.path !== '/login') {
     return navigateTo('/login');
-  } else if (isAuthenticated && to.path === '/login') {
+  } else if (isAuthenticated && to.path === '/' || to.path === '/login') {
     return navigateTo('/users');
   }
 });
